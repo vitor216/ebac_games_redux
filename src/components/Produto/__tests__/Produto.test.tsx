41e2396cbbf0d47bfbf3646eac10efbd7659091a
vitor <eletrotecvitor@gmail.com>
2023-11-07 15:42:38 -0300

    Aula de mock service

---
+++ src/components/Produto/__tests__/Produto.test.tsx
@@ -0,0 +1,29 @@
+import { fireEvent, screen } from '@testing-library/react'
+
+import Produto from '..'
+import { renderizaComProvider } from '../../../Utils/tests'
+
+const jogo = {
+  id: 2,
+  categoria: 'RPG',
+  imagem: '',
+  plataformas: ['Windows', 'PS5', 'Xbox Series S/X'],
+  preco: 199.9,
+  precoAntigo: 299.9,
+  titulo: 'Hogwarts Legacy'
+}
+
+describe('Testes para o componente Produto', () => {
+  test('deve renderizar corretamente', () => {
+    renderizaComProvider(<Produto game={jogo} />)
+    expect(screen.getByText('Hogwarts Legacy')).toBeInTheDocument()
+  })
+
+  test('deve adicionar um item ao carrinho', () => {
+    const { store } = renderizaComProvider(<Produto game={jogo} />)
+    const botao = screen.getByTestId('btn-adicionar-produto')
+    fireEvent.click(botao)
+
+    expect(store.getState().carrinho.itens).toHaveLength(1)
+  })
+})
